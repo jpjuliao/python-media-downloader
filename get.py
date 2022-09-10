cc5fa9f69a124be7a3f394ec2504768b488e4f1e
Juan Pablo Juliao <jpjuliao@gmail.com>
2022-09-10 17:14:30 -0500

    add start and end params

--- get.py
+++ get.py
@@ -5,18 +5,38 @@ from pytube.cli import on_progress
 from pprint import pprint
 import sys
  
+print('Loading...')
+
+# file_path = '/Users/jpjuliao/Projects/python-youtube-downloader/twenty one pilots Heathens (from Suicide Squad The Album) [OFFICIAL VIDEO].mp4'
+# video = VideoFileClip(file_path).subclip(0,30)
+# video.write_videofile("edited.mp4",fps=25)
+
+pprint(globals())
+sys.exit()
+
 url = sys.argv[1]
-pprint(url)
+start = sys.argv[2]
+end = sys.argv[3]
 
-def progress_function(stream, chunk, file_handle, bytes_remaining):
-    print(round((1-bytes_remaining/video.filesize)*100, 3), '% done...')
+# pprint(url)
 
-yt = YouTube(url, on_progress_callback=on_progress)
-pprint(yt)
+def complete_function(stream, file_path):
+    print('Download complete:', file_path)
 
-yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    video = VideoFileClip(file_path).subclip(0,30)
+    video.write_videofile("edited.mp4",fps=25)
 
-# yt.streams.first().download('/Users/jpjuliao/Projects/python-youtube-downloader')
+yt = YouTube(
+    url, 
+    on_progress_callback=on_progress, 
+    on_complete_callback=complete_function
+)
 
-# video = VideoFileClip("myHolidays.mp4").subclip(50,60)
-# video.write_videofile("myHolidays_edited.webm",fps=25)
+# pprint(yt)
+
+print('Downloading: ', yt.title, '~ viewed', yt.views, 'times.')
+
+yt.streams.filter(
+  progressive=True, 
+  file_extension='mp4'
+).order_by('resolution').desc().first().download()
